7a5555524c0f8216e151d1f6f8e114298d2e403f
etienne-chrp <66628513+etienne-chrp@users.noreply.github.com>
2020-11-24 10:50:27 +0100

    Add console project

---
+++ src/dns-tls-proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dns_tls_proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
